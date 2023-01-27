e0206ccf33a3b059e2b15be50d451914c677487f
Ladi Andrew <ladidawodu@gmail.com>
2023-01-27 09:42:58 +0000

    call to action

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='jump__cta'>
+      <div className='jump__cta-content'>
+        <p>Everything stands out, when you follow the road for everyone</p>
+        <h3>A premium service, for every user. That's design, it's time to see creativity in the world of logic </h3>  
+      </div>
+        <div className='jump__cta-btn'>
+        <button type='button'>start here</button> 
+        </div>
+    </div>
   )
 }
 
--- src/containers/possibility/possibility.jsx
+++ src/containers/possibility/possibility.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import './possibility.css';
 import logos  from '../../assests/strong.png';
+
 const Possibility = () => {
   return (
     <div className='jump__possibility section__padding' id="consulting">
       <div className='jump__possibility-image'>
         <img src={logos} alt="consulting" />
       </div>
+
       <div className='jump__possibility-content'>
+ 
         <h4>Think outside the box, we'll design the rest.</h4>
-        <h1>Creativity is destination, imagination is a standard.</h1>
-        <p>Whether it's opposite, dynamic or straight, creativity is a road through boundaries. Let us walk down that road together</p>
+        <h1>Creativity is destination, <br /> imagination is a standard.</h1>
+        <p>Whether it's opposite, <br /> dynamic or straight, creativity is a road through boundaries. <br /> Let us walk down that road together...</p>
+        <h4>Think outside the box...</h4>
+
+
       </div>
+
+
     </div>
   )
 };
 
-export default Possibility
+export default Possibility;
