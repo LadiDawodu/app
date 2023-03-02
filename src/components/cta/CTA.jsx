05f11e893079fc152e23322d670a5cc37f6f0e58
Ladi Andrew <ladidawodu@gmail.com>
2023-03-02 11:51:46 +0000

    buttons

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -12,7 +12,7 @@ import { useState } from 'react';
     <div className='jump__cta'>
       <div className='jump__cta-content'>
         <p>Everything stands out, when you follow the road for everyone</p>
-        <h3>A premium service, for every user. That's design, it's time to see creativity in the world of logic </h3>  
+        <h3>A premium service, for every user.<br/> That's design, it's time to see creativity in the world of logic </h3>  
       </div>
         <div className='jump__cta-btn'>
           
